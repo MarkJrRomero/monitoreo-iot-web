46bc5632f0d824edf5662d9ef8cea9524fe53f41
DevMarkJr <202454293+MarkJrRomero@users.noreply.github.com>
2025-07-10 21:31:04 -0500

    Iniciando el router y primeras vistas básicas

---
+++ src/pages/Auth.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Auth: React.FC = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido a la página de autenticación!</h1>
+    </div>
+  );
+};
+
+export default Auth;
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Auth from '../pages/Auth';
+
+const AppRouter: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default AppRouter;
